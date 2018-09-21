95f656b804d57b0e231e103ea21625c7df5c49aa
SuperCoolGuy855 <29012883+SuperCoolGuy855@users.noreply.github.com>
2018-09-21 18:02:33 +0700

    Replace empty GameObject with O/X GameObject prefabs

--- Caro/Assets/Scripts/GameController.cs
+++ Caro/Assets/Scripts/GameController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
-	// TODO: Add a win detection system
-	private GameObject[][] gameObjectGrid;
-	private bool status = false; //false: O, true: X
-	public GameObject O; // O GameObject prefab
-	public GameObject X; // X GameObject prefab
+public class GameController : MonoBehaviour
+{
+    // TODO: Add a win detection system
+    private GameObject[][] gameObjectGrid;
+    private bool status = false; //false: O, true: X
+    public GameObject O; // O GameObject prefab
+    public GameObject X; // X GameObject prefab
     public float space; // Space between center of each square
-	public Vector2 startPos; // The first position of the square
-	public Vector2 gridSize; // The grid size
-	void Start () {
-		// Declare an empty GameObject grid
+    public Vector2 startPos; // The first position of the square
+    public Vector2 gridSize; // The grid size
+    void Start()
+    {
+        // Declare an empty GameObject grid
         gameObjectGrid = new GameObject[(int)gridSize.x][];
         for (int x = 0; x < gridSize.x; x++)
         {
@@ -21,16 +23,17 @@ public class GameController : MonoBehaviour {
             {
                 GameObject go = new GameObject(x.ToString() + " " + y.ToString());
                 gameObjectGrid[x][y] = go;
-				BoxCollider2D col = go.AddComponent<BoxCollider2D>();
-				col.size = new Vector2(0.4f, 0.4f);
-				gameObjectGrid[x][y].transform.position = new Vector2(startPos.x + (space * x), startPos.y + (space * -y));
-				// Instantiate(O, gameObjectGrid[x][y].transform.position, Quaternion.identity);
+                BoxCollider2D col = go.AddComponent<BoxCollider2D>();
+                col.size = new Vector2(0.4f, 0.4f);
+                gameObjectGrid[x][y].transform.position = new Vector2(startPos.x + (space * x), startPos.y + (space * -y));
+                // Instantiate(O, gameObjectGrid[x][y].transform.position, Quaternion.identity);
             }
-		}
-	}
-	
-	void Update () {
-		// Detect what GameObject have been clicked
+        }
+    }
+
+    void Update()
+    {
+        // Detect what GameObject have been clicked
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -40,14 +43,28 @@ public class GameController : MonoBehaviour {
 
             if (hit.collider != null)
             {
-                if (status) {
-                    Instantiate(X, hit.collider.gameObject.transform.position, Quaternion.identity);
-				} else {
-                    Instantiate(O, hit.collider.gameObject.transform.position, Quaternion.identity);
-				}
-				status = ! status;
-                Destroy(hit.collider.gameObject);
+                for (int x = 0; x < gridSize.x; x++)
+                {
+                    for (int y = 0; y < gridSize.y; y++)
+                    {
+                        if (hit.collider.gameObject.name == gameObjectGrid[x][y].name)
+                        {
+                            if (status)
+                            {
+                                gameObjectGrid[x][y] = Instantiate(X, hit.collider.gameObject.transform.position, Quaternion.identity);
+                                gameObjectGrid[x][y].name = "X";
+                            }
+                            else
+                            {
+                                gameObjectGrid[x][y] = Instantiate(O, hit.collider.gameObject.transform.position, Quaternion.identity);
+                                gameObjectGrid[x][y].name = "O";
+                            }
+                            status = !status;
+                            Destroy(hit.collider.gameObject);
+                        }
+                    }
+                }
             }
         }
-	}
+    }
 }
