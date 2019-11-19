ff78ca38d48aa1c8e902ce2fa222e6cf1e032c62
SuperCoolGuy855 <minhminh99599@gmail.com>
2019-11-19 23:36:04 +0700

    Update

---
+++ FPS/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player_transform;
+    private Transform camera_transform;
+    private Vector3 offset;
+    void Start()
+    {
+        camera_transform = GetComponent<Transform>();
+        offset = camera_transform.position - player_transform.position;
+        Debug.Log(offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera_transform.position = player_transform.position + offset;
+    }
+}
---
+++ FPS/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxis ("Horizontal");
+        float ver = Input.GetAxis ("Vertical");
+        rb.AddForce(new Vector3(hor * speed, 0, ver * speed));
+    }
+}
